ce8c19eaf9b227635e0405a5b7d87002e8340b80
carterGarcia <59673486+carterGarcia@users.noreply.github.com>
2022-10-16 16:49:36 -0700

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -36,6 +36,24 @@ public class GameBoard {
         }
     }
 
+    public boolean checkWin(){
+        if(gameBoard[0][0] == gameBoard[1][1] & gameBoard[2][2] == gameBoard[1][1]){
+            return true;
+        }
+        if(gameBoard[0][2] == gameBoard[1][1] & gameBoard[0][2] == gameBoard[1][1]){
+            return true;
+        }
+        for (int i = 0; i > 2; i++){
+                if(gameBoard[i][0] == gameBoard[i][1] & gameBoard[i][1] == gameBoard[i][2]){
+                    return true;
+                }
+                if(gameBoard[0][i] == gameBoard[1][i] & gameBoard[1][i] == gameBoard[2][i]){
+                    return true;
+                }
+            }
+            return false;
+        }
+
     public void drawBoard(){
         /*
         print the board including the current moves
