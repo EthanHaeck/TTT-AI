75ff5bad49a940ad46bbc12d8b0012b6e75aa8a8
Ethan-Haeck <haecke@wwu.edu>
2022-10-16 16:06:55 -0700

    Game flow stuff

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -19,11 +19,22 @@ public class PlayGame {
         int opponentChoice = userInput.nextInt();
 
         //Handle different choices
-
-        // loop until the game is finished, alternating turns
-        while(true){ // <-- CHANGE LATER
-            board.drawBoard();
-            playerTurn(currentPlayer);
+        if(opponentChoice == 1){ // PLAYER VS AI
+            // loop until the win condition is reached, alternating turns
+            while(true){ // <-- CHANGE LATER
+                board.drawBoard();
+                playerTurn();
+                board.drawBoard();
+                AITurn(2);
+            }
+        }
+        else{ //AI VS AI
+            while(true){
+                board.drawBoard();
+                AITurn(1);
+                board.drawBoard();
+                AITurn(2);
+            }
         }
 
         // Display game results (Win, Lose, Tie)
@@ -31,25 +42,35 @@ public class PlayGame {
 
     }
 
-    private void playerTurn(int currentPlayer){
+    private void playerTurn(){
         /*
         handles logic of a player's turn (placing X/O, checking validity of move)
         */
         int selectedRow;
         int selectedCol;
 
-        System.out.printf("***Player %d's turn!***\n", currentPlayer);
-        System.out.println("Enter row [0 to 2]");
+        System.out.println("***Player 1's turn!***");
+        System.out.print("Enter row [0 to 2]: ");
         selectedRow = userInput.nextInt();
-        System.out.println("Enter col [0 to 2]");
+        System.out.print("Enter col [0 to 2]: ");
         selectedCol = userInput.nextInt();
 
+        // --- OPTIMIZE LATER ---
         // check validity and check move
-        board.tryPlacePiece(selectedRow, selectedCol);
+        while(!board.tryPlacePiece(selectedRow, selectedCol)){
+            System.out.println("---Move is invalid, try again!---");
+            System.out.print("Enter row [0 to 2]: ");
+            selectedRow = userInput.nextInt();
+            System.out.print("Enter col [0 to 2]: ");
+            selectedCol = userInput.nextInt();
+        }
 
     }
 
-    private void AITurn(){
+    private void AITurn(int playerNum){
+        System.out.printf("***Player %d's turn!***\n", playerNum);
+
+
 
     }
 
