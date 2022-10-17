c417260dd8bdc071ee22bd497d6db015d9afde8d
carterGarcia <59673486+carterGarcia@users.noreply.github.com>
2022-10-16 17:27:54 -0700

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -37,17 +37,29 @@ public class GameBoard {
     }
 
     public boolean checkWin(){
-        if(gameBoard[0][0] == gameBoard[1][1] & gameBoard[2][2] == gameBoard[1][1]){
+        if(gameBoard[0][0] == 'X' & gameBoard[1][1] == 'X' & gameBoard[2][2] == 'X'){
             return true;
         }
-        if(gameBoard[0][2] == gameBoard[1][1] & gameBoard[0][2] == gameBoard[1][1]){
+        if(gameBoard[0][0] == 'O' & gameBoard[1][1] == 'O' & gameBoard[2][2] == 'O'){
+            return true;
+        }
+        if(gameBoard[0][2] == 'X' & gameBoard[1][1] == 'X' & gameBoard[2][0] == 'X'){
+            return true;
+        }
+        if(gameBoard[0][2] == 'O' & gameBoard[1][1] == 'O' & gameBoard[2][O] == 'O'){
             return true;
         }
         for (int i = 0; i > 2; i++){
-                if(gameBoard[i][0] == gameBoard[i][1] & gameBoard[i][1] == gameBoard[i][2]){
+                if(gameBoard[i][0] == 'X' & gameBoard[i][1] == 'X' & gameBoard[i][2] == 'X'){
                     return true;
                 }
-                if(gameBoard[0][i] == gameBoard[1][i] & gameBoard[1][i] == gameBoard[2][i]){
+                if(gameBoard[i][0] == 'O' & gameBoard[i][1] == 'O' & gameBoard[i][2] == 'O'){
+                    return true;
+                }
+                if(gameBoard[0][i] == 'X' & gameBoard[1][i] == 'X' & gameBoard[2][i] == 'X'){
+                    return true;
+                }
+                if(gameBoard[0][i] == 'O' & gameBoard[1][i] == 'O' & gameBoard[2][i] == 'O'){
                     return true;
                 }
             }
