f59389b84db18f5c020ea995b326cecd6a0681e0
Ethan-Haeck <haecke@wwu.edu>
2022-10-16 19:14:47 -0700

    AI turn, checking for wins, etc

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class GameBoard {
     private static final char X = 'X';
     private static final char O = 'O';
@@ -6,13 +8,6 @@ public class GameBoard {
                                  ,{EMPTY,EMPTY,EMPTY}
                                  ,{EMPTY,EMPTY,EMPTY}};
 
-    //possible methods:
-    /*
-        - tryPlacePiece
-        - checkWin
-        - evaluate
-        - drawBoard
-     */
 
     public boolean tryPlacePiece(int row, int col){
         /*
@@ -36,35 +31,55 @@ public class GameBoard {
         }
     }
 
-    public boolean checkWin(){
+
+    public int checkWin(){
+        // NO WIN = 0
+        // P1 WINS = 1
+        // P2 WINS = 2
+        // TIE = 3
+
+        // diagonal cases
         if(gameBoard[0][0] == 'X' & gameBoard[1][1] == 'X' & gameBoard[2][2] == 'X'){
-            return true;
+            return 1;
         }
         if(gameBoard[0][0] == 'O' & gameBoard[1][1] == 'O' & gameBoard[2][2] == 'O'){
-            return true;
+            return 2;
         }
         if(gameBoard[0][2] == 'X' & gameBoard[1][1] == 'X' & gameBoard[2][0] == 'X'){
-            return true;
+            return 1;
         }
-        if(gameBoard[0][2] == 'O' & gameBoard[1][1] == 'O' & gameBoard[2][O] == 'O'){
-            return true;
+        if(gameBoard[0][2] == 'O' & gameBoard[1][1] == 'O' & gameBoard[2][0] == 'O'){
+            return 2;
         }
-        for (int i = 0; i > 2; i++){
-                if(gameBoard[i][0] == 'X' & gameBoard[i][1] == 'X' & gameBoard[i][2] == 'X'){
-                    return true;
-                }
-                if(gameBoard[i][0] == 'O' & gameBoard[i][1] == 'O' & gameBoard[i][2] == 'O'){
-                    return true;
-                }
-                if(gameBoard[0][i] == 'X' & gameBoard[1][i] == 'X' & gameBoard[2][i] == 'X'){
-                    return true;
-                }
-                if(gameBoard[0][i] == 'O' & gameBoard[1][i] == 'O' & gameBoard[2][i] == 'O'){
-                    return true;
+
+        //rows and columns
+        for (int i = 0; i < 2; i++){
+            if(gameBoard[i][0] == 'X' & gameBoard[i][1] == 'X' & gameBoard[i][2] == 'X'){
+                return 1;
+            }
+            if(gameBoard[i][0] == 'O' & gameBoard[i][1] == 'O' & gameBoard[i][2] == 'O'){
+                return 2;
+            }
+            if(gameBoard[0][i] == 'X' & gameBoard[1][i] == 'X' & gameBoard[2][i] == 'X'){
+                return 1;
+            }
+            if(gameBoard[0][i] == 'O' & gameBoard[1][i] == 'O' & gameBoard[2][i] == 'O'){
+                return 2;
+            }
+        }
+
+        //check if moves can still be made
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                if(gameBoard[i][j] == EMPTY){
+                    return 0;
                 }
             }
-            return false;
         }
+
+        //no winner and no moves can be played -- game ties
+        return 3;
+    }
 
     public void drawBoard(){
         /*
@@ -75,6 +90,38 @@ public class GameBoard {
         System.out.printf(" %c | %c | %c \n", gameBoard[1][0], gameBoard[1][1], gameBoard[1][2]);
         System.out.println("---+---+---");
         System.out.printf(" %c | %c | %c \n", gameBoard[2][0], gameBoard[2][1], gameBoard[2][2]);
+    }
+
+    public void placeAIPiece(){
+        // places the AI's move on the board
+        int selectedRow;
+        int selectedCol;
+
+        //evaluate the current board
+
+        //choose a position
+
+
+        // TEMPORARY - JUST FOR TESTING
+        Random random = new Random();
+        selectedRow = random.nextInt(3);
+        selectedCol = random.nextInt(3);
+        while(gameBoard[selectedRow][selectedCol] != EMPTY){
+            selectedRow = random.nextInt(3);
+            selectedCol = random.nextInt(3);
+        }
+
+        //place the piece
+        gameBoard[selectedRow][selectedCol] = O;
+
+        //print the move
+        System.out.printf("%d, %d\n", selectedRow, selectedCol);
+    }
+
+    private void evaluate(){
+        //Returns higher numbers if player 1 is at an advantage
+        //or lower numbers if player 2 is at an advantage
+
     }
 
 }
